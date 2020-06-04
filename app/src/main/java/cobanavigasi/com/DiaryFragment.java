a4a0380176a5fe99076422cbf32ae517179d10d5
Rosyaifa Meifani Pratami <rosyaaaa28@gmail.com>
2020-06-05 00:43:48 +0700

    tambahan tombol timeline pada diary fragment

--- app/src/main/java/cobanavigasi/com/DiaryFragment.java
+++ app/src/main/java/cobanavigasi/com/DiaryFragment.java
@@ -13,7 +13,7 @@ import androidx.fragment.app.Fragment;
 public class DiaryFragment extends Fragment {
 
     private DiaryViewModel diaryViewModel;
-    private Button btn;
+    private Button btn, btnCal;
 
 
 
@@ -24,6 +24,15 @@ public class DiaryFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState){
         View root = inflater.inflate(R.layout.diary_fragment, container, false);
+        btnCal = root.findViewById(R.id.btn_cal);
+        btnCal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), Calendar.class);
+                startActivity(intent);
+            }
+        });
+
         btn= root.findViewById(R.id.buttonNote);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
